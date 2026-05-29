39743ffb74c58a03f97702a90bb939af14a7f55f
13607fatima <13607@holbertonschool.com>
2026-05-29 13:06:32 +0000

    task

---
+++ network_security/2x00_network_fundamentals/gitpush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERNAME="13607fatima"
+TOKEN="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"
+
+REPO_URL=$(git remote get-url origin | sed 's|https://github.com/||' | sed 's|git@github.com:||')
+
+git add .
+git commit -m "task"
+
+git push "https://${USERNAME}:${TOKEN}@github.com/${REPO_URL}" main
